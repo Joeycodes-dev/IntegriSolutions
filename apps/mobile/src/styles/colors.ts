6ab650c769d2ff13d97d3d8fa2c287bbab32c1f2
Skhulile28 <82827594+Skhulile28@users.noreply.github.com>
2026-05-20 23:21:37 +0200

    Created UI Colours file
    main colours of the mobile app

---
+++ apps/mobile/src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primaryDark: '#0D253F',
+  accentBlue: '#4096FE',
+  neutralGray: '#9BA5B7',
+  inputBorder: '#102A43',
+  background: '#FFFFFF',
+  textPrimary: '#102A43',
+  textSecondary: '#334E68',
+};
